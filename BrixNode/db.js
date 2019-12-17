bafe9cef706d1fc68ee68d202e7b32b439c04bce
nadim-khan <khan.nadim58@gmail.com>
2019-12-18 00:15:16 +0530

    first commit

---
+++ BrixNode/db.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const mongoose=require('mongoose');
+
+dbUrl='mongodb://localhost:27017/CrudDB';
+
+var currentdate = new Date();
+var date = "Last Sync: " + currentdate.getDay() + "/" + currentdate.getMonth() 
++ "/" + currentdate.getFullYear() + " @ " 
++ currentdate.getHours() + ":" 
++ currentdate.getMinutes() + ":" + currentdate.getSeconds();
+
+
+
+mongoose.connect(dbUrl,(err)=>{
+    if(!err){
+        console.log("\n["+date+"] - Succesfully connected to DB ["+dbUrl+"] ");
+    }else{
+        console.log("\n["+date+"] - Error connecting to DB "+JSON.stringify(err,undefined,2));
+    }
+})
+
+module.exports=mongoose;
